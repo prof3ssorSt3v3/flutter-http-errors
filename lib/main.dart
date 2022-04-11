2656bf7a2bc40496564658b6b22d8bcd76f43502
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-04-11 16:26:58 -0400

    updated the title

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('App Title'),
+        title: Text('Handling Http Requests'),
       ),
       body: Column(
         mainAxisSize: MainAxisSize.max,
